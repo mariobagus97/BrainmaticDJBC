7033c19bddeb493f736a00cd1112f5a281a7b624
M. Ario Bagus <>
2022-07-10 22:21:08 +0700

    day 1

---
+++ DJBC/DJBC/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  DJBC
+//
+//  Created by SehatQ on 10/07/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
+
+}
+
